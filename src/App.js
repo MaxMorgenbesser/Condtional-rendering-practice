99745e81be29fe00efb3e263a61cdd36efa200b8
Max Morgenbesser <maxwellmorgenbesser@gmail.com>
2022-08-08 13:03:57 -0400

    Practiced using conditional rendering

--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
 import logo from './logo.svg';
 import './App.css';
+import DogPic from './DogPic';
+import SheepPic from './SheepPic';
+import {useState} from 'react'
+import CatPic from './CatPic';
 
 function App() {
+  const [dog,setDog]=useState(false)
+  const [sheep,setSheep]=useState(false)
+  const [cat,setCat]=useState(false)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+
+   <button onClick={()=> {
+    setDog(!dog)
+   }}>Show the Dog!</button>
+   <h2>{dog ? <DogPic/> : <h1>Show a dog!</h1>
+
+   }</h2>
+   <button onClick={()=> {
+    setSheep(!sheep)
+   }}>Show the Sheep!</button>
+   <h2>{sheep ? <SheepPic/> : <h1>Show a sheep!</h1>
+
+   }</h2>
+
+<button onClick={()=> {
+    setCat(!cat)
+   }}>Show a cat!</button>
+   <h2>{cat ? <CatPic/> : <h1>Show a cat!</h1>
+
+   }</h2>
+
+
+   </>
   );
 }
 
